26ece8c5ee9b33a7a9507cde8ffb08171370d686
zhyncs <me@zhyncs.com>
2022-03-09 23:39:19 +0800

    add g_zh and sso_ua script

---
+++ script/g_zh.js
@@ -0,0 +1,3 @@
+let url = $request.url;
+url = url + "&hl=zh-cn";
+$done({ url });
---
+++ script/sso_ua.js
@@ -0,0 +1,4 @@
+let headers = $request.headers;
+headers["User-Agent"] =
+  "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25";
+$done({ headers });
